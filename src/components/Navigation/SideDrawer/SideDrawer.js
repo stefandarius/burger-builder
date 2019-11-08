30f58ecb27cde614a0abc392fdcb37530ac1596d
danuberobotics <danuberobotics@gmail.com>
2019-11-08 14:25:02 +0200

    cdsc

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+
+};
+
+export default sideDrawer;
--- src/components/UI/Navigation/SideDrawer/SideDrawer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const sideDrawer = (props) => (
-
-);
-
-export default sideDrawer;
