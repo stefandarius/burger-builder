1d081d0fe1d8654fe6d51fad162fbd896b621eed
danuberobotics <danuberobotics@gmail.com>
2019-12-12 09:02:11 +0200

    fianl commit

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from '../DrawerToggle/DrawerToggle.module.css';
 
 const drawerToglle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToglle;
